ef292c07161bfa8cffcf70d66f396491ad513e8f
James Benner <james.benner@gmail.com>
2016-07-05 20:13:32 -0400

    PhpSpec fixes.

--- spec/Games/SchemaOrgGameSpec.php
+++ spec/Games/SchemaOrgGameSpec.php
@@ -4,9 +4,22 @@ namespace spec\VideoGames\Games;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use stdClass;
 
 class SchemaOrgGameSpec extends ObjectBehavior
 {
+    function let()
+    {
+        $wikidataGame = (object) [
+            'name'      => '',
+            'platform'  => '',
+            'publisher' => '',
+            '_game'     => ''
+        ];
+
+        $this->beConstructedWith($wikidataGame);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('VideoGames\Games\SchemaOrgGame');
